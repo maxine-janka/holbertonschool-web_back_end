370dac9cd2bccbdcf581fc6b7db15f91c6db7c3e
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-03-31 21:17:03 +1100

    disable es lint and add comment

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -2,9 +2,15 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
-	if (trueOrFalse) {
+  if (trueOrFalse) {
+    /* eslint-disable no-unused-vars */
     const task = true;
     const task2 = false;
+    /* eslint-disable no-unused-vars */
   }
+  
   return [task, task2];
 }
+
+/* const in if block does not reassign const outer varibles it shadows them
+they are separate varibales that only exist in the if block */
