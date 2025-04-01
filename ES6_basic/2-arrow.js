c00d3cf7715236c6aa32d285fafef23af1e4f6b2
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-04-01 11:29:05 +1100

    Fix syntax

--- ES6_basic/2-arrow.js
+++ ES6_basic/2-arrow.js
@@ -2,7 +2,7 @@ export default function getNeighborhoodsList() {
     this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
     const self = this;
-    this.addNeighborhood = newNeighborhood => {
+    this.addNeighborhood = (newNeighborhood) => {
       self.sanFranciscoNeighbosrhoods.push(newNeighborhood);
       return self.sanFranciscoNeighborhoods;
     };
