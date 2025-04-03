09089b92df77cc1ab93dfe055640298e5d09be15
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-04-03 15:19:48 +1100

    Fix: eslint

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,9 +1,9 @@
 export default function createReportObject(employeesList) {
   const reportObject = {
-    allEmployees:  {...employeesList,},
-    getNumberOfDepartments () {
+    allEmployees: { ...employeesList },
+    getNumberOfDepartments() {
       return Object.keys(employeesList).length;
-      },  
+    },
   };
   return reportObject;
 }
