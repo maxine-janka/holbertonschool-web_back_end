fd30ae121f73191c5502d8a77542c0f927cd5c01
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-04-03 15:17:27 +1100

    Create report object

---
+++ ES6_basic/12-createReportObject.js
@@ -0,0 +1,9 @@
+export default function createReportObject(employeesList) {
+  const reportObject = {
+    allEmployees:  {...employeesList,},
+    getNumberOfDepartments () {
+      return Object.keys(employeesList).length;
+      },  
+  };
+  return reportObject;
+}
