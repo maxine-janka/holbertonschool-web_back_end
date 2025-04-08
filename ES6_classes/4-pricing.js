bf84bdd6ded48483b33f49058115879a1e811f6e
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-04-08 16:58:35 +1000

    More methods and static methods

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,43 @@
+import Currency from "./3-currency.js";
+
+export default class Pricing {
+  constructor(amount, currency) { // Passing in an instance of the Currency class
+    this._amount = amount;
+    this._currency = currency
+    }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (typeof value === 'number') {
+      this._amount = value    
+    } else {
+      throw new TypeError('Amount must be a number');
+    }
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    if (value instanceof Currency) {
+      this._currency = value;
+    } else {
+      throw new TypeError('Currency must be an instance of the Class Currency')
+    }
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this.currency.name} (${this.currency.code})`
+  }
+
+  // Don't need 'this'. Belongs to the class itself not an instance.
+  // Therefore no instance _amount to pull from.
+  // Pricing.convertprice() not price.convertPrice()
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }  
+}
