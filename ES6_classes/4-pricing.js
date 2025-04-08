d9a209aeee425c7309eca9e60d483bd626a408bc
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-04-08 17:00:28 +1000

    Fix lint

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,10 +1,10 @@
-import Currency from "./3-currency.js";
+import Currency from './3-currency';
 
 export default class Pricing {
   constructor(amount, currency) { // Passing in an instance of the Currency class
     this._amount = amount;
-    this._currency = currency
-    }
+    this._currency = currency;
+  }
 
   get amount() {
     return this._amount;
@@ -12,7 +12,7 @@ export default class Pricing {
 
   set amount(value) {
     if (typeof value === 'number') {
-      this._amount = value    
+      this._amount = value; 
     } else {
       throw new TypeError('Amount must be a number');
     }
@@ -26,12 +26,12 @@ export default class Pricing {
     if (value instanceof Currency) {
       this._currency = value;
     } else {
-      throw new TypeError('Currency must be an instance of the Class Currency')
+      throw new TypeError('Currency must be an instance of the Class Currency');
     }
   }
 
   displayFullPrice() {
-    return `${this._amount} ${this.currency.name} (${this.currency.code})`
+    return `${this._amount} ${this.currency.name} (${this.currency.code})`;
   }
 
   // Don't need 'this'. Belongs to the class itself not an instance.
@@ -39,5 +39,5 @@ export default class Pricing {
   // Pricing.convertprice() not price.convertPrice()
   static convertPrice(amount, conversionRate) {
     return amount * conversionRate;
-  }  
+  }
 }
