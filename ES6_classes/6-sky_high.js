ec7233dfa31a678c798ceceed8ab00f8d47bcdaf
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-04-10 10:52:48 +1000

    Invoking parent class with super

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,25 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors; 
+    }
+
+    get floors() {
+      return this._floors;
+    }
+
+    set floors(value) {
+      if (typeof value === 'number') {
+        this._floors = value;
+      } else {
+        throw new TypeError('floors must be a number');
+      }
+    }
+    
+    // Use this.floors so it doesn't bypass the getter and setter
+    evacuationWarningMessage() {
+      return `Evacuate slowly the ${this.floors} floors`;
+  }
+}
