6bee91aa2f33f0802a93f3764ba094d8766fdea0
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-04-10 14:15:43 +1000

    cloning objects

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,49 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this.motor = motor;
+    this.color = color;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  set brand(value) {
+    if (typeof value === 'string' || value === undefined) {
+      this._brand = value;
+    } else {
+      throw new TypeError('brand must be a string or undefined')
+    }
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  set motor(value) {
+    if (typeof value === 'string'  || value === undefined) {
+      this._motor = value;
+    } else {
+      throw new TypeError('motor must be a string or undefined')
+    }
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  set color(value) {
+    if (typeof value === 'string' || value === undefined) {
+      this._color = value;
+    } else {
+      throw new TypeError('color must be a string or undefined')
+    }
+  }
+
+  // Creates a new instabce of the same type
+  // this = current instance, constructor = points to class/method that created it
+  cloneCar() {
+    return new this.constructor();
+  }
+}
