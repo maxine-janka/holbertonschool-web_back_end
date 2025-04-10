4ae199d08c924a54d7d3e7734e7b43292811fdd1
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-04-10 11:18:38 +1000

    Fix lint

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -3,23 +3,23 @@ import Building from './5-building';
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-    this._floors = floors; 
-    }
+    this._floors = floors;
+  }
 
-    get floors() {
-      return this._floors;
-    }
+  get floors() {
+    return this._floors;
+  }
 
-    set floors(value) {
-      if (typeof value === 'number') {
-        this._floors = value;
-      } else {
-        throw new TypeError('floors must be a number');
-      }
+  set floors(value) {
+    if (typeof value === 'number') {
+      this._floors = value;
+    } else {
+      throw new TypeError('floors must be a number');
     }
-    
-    // Use this.floors so it doesn't bypass the getter and setter
-    evacuationWarningMessage() {
-      return `Evacuate slowly the ${this.floors} floors`;
+  }
+
+  // Use this.floors so it doesn't bypass the getter and setter
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
   }
 }
