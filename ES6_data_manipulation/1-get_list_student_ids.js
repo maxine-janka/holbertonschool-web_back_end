f38a8595e107e0d1bf210c9b81f685117285451a
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-04-11 16:54:56 +1000

    Fix lint

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,6 @@
 export default function getListStudentIds(arr) {
- if (Array.isArray(arr)) {
+  if (Array.isArray(arr)) {
     const idArr = arr.map(({ id }) => id);
     return idArr;
- } else
-   return [];
+  } return []; 
 }
