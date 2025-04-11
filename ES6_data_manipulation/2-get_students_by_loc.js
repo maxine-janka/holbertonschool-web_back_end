5c1e7a82c44bbdc07325365f00ca1d206c29131f
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-04-11 17:24:22 +1000

    Fix lint

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,5 +1,5 @@
 export default function getStudentsByLocation(studentList, city) {
-  // Iterate each 'student' object in the array, 
+  // Iterate each 'student' object in the array,
   // access the location property and compare to input city
   const filteredList = studentList.filter((student) => student.location === city);
   return filteredList;
