803102da4cefa143e2455de51fcf886637f3bae4
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-04-12 12:00:00 +1000

    Use reduce to sum object property

---
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,4 @@
+export default function getStudentIdsSum(studentList) { 
+  const sumOfIds =  studentList.reduce((accumulator, currentValue) => accumulator + currentValue.id, 0);
+  return sumOfIds;
+}
