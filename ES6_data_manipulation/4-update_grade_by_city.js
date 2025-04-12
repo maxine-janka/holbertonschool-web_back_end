046cff096f6e046b5c8a470ea70855ee83a5e34e
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-04-12 13:36:40 +1000

    Fix lint

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,25 +1,28 @@
 export default function updateStudentGradeByCity(studentList, city, newGrades) {
+  // New arr with filtered students from specified city
   const studentsInCity = studentList
-    .filter(student => student.location === city); // New arr with filtered students from specified city
+    .filter((student) => student.location === city); 
 
   // From filtered list create new array
-  // for each student in filtered list, search the newGrades input array, if there is a match with student id's, store the grade object in studentGradeObject
-  const updatedStudents = studentsInCity.map(student => {
-    const studentGradeObject = newGrades.find(grade => grade.studentId === student.id);
+  // for each student in filtered list, search the newGrades input array,
+  // if there is a match with student id's, store the grade object in studentGradeObject
+  const updatedStudents = studentsInCity.map((student) => {
+    const studentGradeObject = newGrades.find((grade) => grade.studentId === student.id);
 
-    // If grade object exists from prev match, take the grade property and store in grade, otherwise set as NA
+    // If grade object exists from prev match, take the grade property and store in grade,
+    // Otherwise set as NA
     let grade;
     if (studentGradeObject) {
-      grade = studentGradeObject.grade
+      grade = studentGradeObject.grade;
     } else {
       grade = 'N/A';
     }
-    
+
     // Use original student data and add grade property to create newStudent Object
     // for current student
     const newStudent = {
       ...student,
-      grade: grade
+      grade: grade,
     };
 
     return newStudent;
