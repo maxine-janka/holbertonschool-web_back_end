8ecef90e4b9f2460277ee0e21d03b5efa05a6a3c
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-04-13 17:33:21 +1000

    Fix lint

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -3,14 +3,14 @@ export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   // Create Dataview to write to buffer
   const view = new DataView(buffer);
-  
+
   // Check valid range to set 8bit signed int at specified position
   if (position <= length - 1) {
     view.setInt8(position, value);
   } else {
     throw new Error('Position outside range');
   }
-  //return DataView object
-  //console.log(view.getInt8(2));
+  // return DataView object
+  // console.log(view.getInt8(2));
   return view;
 }
