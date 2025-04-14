410731e85b30da937d6196f5dfd43b86794d7f84
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-04-14 13:20:10 +1000

    Update values in Map object

---
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,12 @@
+export default function updateUniqueItems(mapObj) {
+  if (mapObj instanceof Map) {
+    for (const [key, value] of mapObj) {
+      if (value === 1) {
+        mapObj.set(key, 100)
+      }
+    }
+  } else {
+    throw new Error('Cannot process')
+  }
+  return mapObj;
+}
