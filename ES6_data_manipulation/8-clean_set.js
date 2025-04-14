79498e0d1b012750475d9d8a45dd5838af4b35c3
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-04-14 12:17:24 +1000

    Fix lint

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,13 +1,12 @@
 export default function cleanSet(set, startString) {
-
-if (typeof startString !== 'string' || startString === '') {
-  return '';
+  if (typeof startString !== 'string' || startString === '') {
+    return '';
 }
   const setToArrayAndFilter = Array.from(set).filter((element) => typeof element === 'string' && element.startsWith(startString));
-  //console.log(setToArrayAndFilter);
+  // console.log(setToArrayAndFilter);
   const sliceStartString = setToArrayAndFilter.map((element) => element.slice(startString.length));
-  //console.log(sliceStartString);
+  // console.log(sliceStartString);
   const newString = sliceStartString.join('-');
-  //console.log(newString);
+  // console.log(newString);
   return newString;
 }
