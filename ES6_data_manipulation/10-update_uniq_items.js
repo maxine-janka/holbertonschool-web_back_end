c4a4a9c18c8f13ae997eb414c9e299c8997bbe14
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-04-14 13:21:19 +1000

    Fix lint

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -2,11 +2,11 @@ export default function updateUniqueItems(mapObj) {
   if (mapObj instanceof Map) {
     for (const [key, value] of mapObj) {
       if (value === 1) {
-        mapObj.set(key, 100)
+        mapObj.set(key, 100);
       }
     }
   } else {
-    throw new Error('Cannot process')
+    throw new Error('Cannot process');
   }
   return mapObj;
 }
