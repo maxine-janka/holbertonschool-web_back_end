8e1c3cb668e4189acccb37561925bc44d9d02fb1
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-04-14 12:15:36 +1000

    Filter substring and return joined string without substring

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,13 @@
+export default function cleanSet(set, startString) {
+
+if (typeof startString !== 'string' || startString === '') {
+  return '';
+}
+  const setToArrayAndFilter = Array.from(set).filter((element) => typeof element === 'string' && element.startsWith(startString));
+  //console.log(setToArrayAndFilter);
+  const sliceStartString = setToArrayAndFilter.map((element) => element.slice(startString.length));
+  //console.log(sliceStartString);
+  const newString = sliceStartString.join('-');
+  //console.log(newString);
+  return newString;
+}
