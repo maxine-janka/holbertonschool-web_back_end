5f9e460a47f328f17134868133fbe2dc41057455
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-04-22 11:13:58 +1000

    Annotated function returns a string from a float

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""This module contains a function that takes a float
+and returns a string representation"""
+
+
+def to_str(n: float) -> str:
+    """Takes a float and returns a string representation"""
+    return str(n)
