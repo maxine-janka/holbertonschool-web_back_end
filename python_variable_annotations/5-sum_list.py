64519ed167698335a99588d40099db19b67e0e42
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-04-22 11:56:44 +1000

    annotated func to sum a list of floats

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""This module contains a function that returns the sum of floats in list"""
+
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Takes a list of floats and returns the sum"""
+    return sum(input_list)
