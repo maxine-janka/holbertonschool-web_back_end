21f1ecf58378e01ba3cb9b4adf35149ccc12f289
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-04-22 11:48:52 +1000

    Annotated variables

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""This module contains annotated variables of different types"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
