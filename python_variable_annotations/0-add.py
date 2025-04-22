a88382de3013adf35e7ecdc5a786ad2a537f02dc
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-04-22 10:43:33 +1000

    fix pystyle

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 """
-A type-annotated function that takes 2 float numbers and returns their sum as a float.
+A type-annotated function that takes 2 float numbers
+and returns their sum as a float.
 """
+
+
 def add(a: float, b: float) -> float:
     """Addition of two float numbers and returns the sum"""
     return a + b
