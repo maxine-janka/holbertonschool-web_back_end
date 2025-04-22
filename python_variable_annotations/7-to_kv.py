3db551f64d76d7f958289e7ab79b7024b400abbf
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-04-22 12:14:24 +1000

    Add docstring

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""This modules contains a function that takes a st
+"""This modules contains an annotated function that takes a st
 and an int OR float and returns a tuple"""
 
 
@@ -7,4 +7,5 @@ from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Takes a str and int or float and returns a tuple"""
     return (k, v ** 2)
