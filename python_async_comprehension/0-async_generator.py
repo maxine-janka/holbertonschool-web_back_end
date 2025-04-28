64383738f280dcb7d010beb5b80f56be8254acc7
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-04-28 15:36:15 +1000

    Add None return param to annoatation

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -8,9 +8,9 @@ from typing import AsyncGenerator
 import random
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> AsyncGenerator[float, None, None]:
     """Yields 10 random floats with 1 second async delay"""
-    
+
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
