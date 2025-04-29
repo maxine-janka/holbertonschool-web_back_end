1b40e62c179dba9f329a11f7b3698bea2a2df979
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-04-29 15:05:31 +1000

    Return a resolved promise

---
+++ ES6_promise/4-user-promise.js
@@ -0,0 +1,7 @@
+export default function signUpUser(firstName, lastName) {
+  const promise = new Promise((resolve, reject) => {
+    resolve ({ firstName, lastName });
+    reject(new Error());
+  });
+  return promise;
+}
