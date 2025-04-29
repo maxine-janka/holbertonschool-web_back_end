306ace7ce7fdcf312a791125c5db69c0a8b397b3
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-04-29 16:01:07 +1000

    Handle multiple promises and map results to new object

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,13 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((arr) => arr.map((obj) => {
+      if (obj.status === 'fulfilled') {
+        return { status: obj.status, value: obj.value };
+      }
+      return { status: obj.status, value: obj.reason }
+    }))
+}
