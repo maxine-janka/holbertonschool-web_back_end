c3111281e4e0eb57f59c52f266bae0bc8423656d
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-04-29 14:40:31 +1000

    Fix eslint

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,10 +1,11 @@
-import { uploadPhoto, createUser } from "./utils";
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()]).then(([uploadPhotoResponse, createUserResponse]) => {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then(([uploadPhotoResponse, createUserResponse]) => {
     console.log(`${uploadPhotoResponse.body} ${createUserResponse.firstName} ${createUserResponse.lastName}`);
   })
-  .catch(() => {
-    console.log('Signup system offline')
-  });
+    .catch(() => {
+      console.log('Signup system offline');
+    });
 }
