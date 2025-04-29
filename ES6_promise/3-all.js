e54a0d5f2845f90944d75e14f7260aa1538dedf6
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-04-29 14:38:16 +1000

    Resolve promises collectively with Promise.all

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,10 @@
+import { uploadPhoto, createUser } from "./utils";
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()]).then(([uploadPhotoResponse, createUserResponse]) => {
+    console.log(`${uploadPhotoResponse.body} ${createUserResponse.firstName} ${createUserResponse.lastName}`);
+  })
+  .catch(() => {
+    console.log('Signup system offline')
+  });
+}
