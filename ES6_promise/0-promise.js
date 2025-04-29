52b0e3e7d2367f4b8cc3153efa0de93cbb1b9654
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-04-29 12:37:16 +1000

    Return a promise

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,10 @@
+export default function getResponseFromAPI() {
+  const promise = new Promise((resolve, reject) => {
+    try {
+      resolve("Success");
+    } catch (error) {
+      reject("error");
+    }
+  });
+  return promise;
+}
