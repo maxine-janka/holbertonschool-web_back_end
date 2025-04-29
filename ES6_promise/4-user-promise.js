15938fdcc3fc10b628b7ace60c9ca9a8e2b416a3
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-04-29 15:09:21 +1000

    Fix eslint

--- ES6_promise/4-user-promise.js
+++ ES6_promise/4-user-promise.js
@@ -1,6 +1,6 @@
 export default function signUpUser(firstName, lastName) {
   const promise = new Promise((resolve, reject) => {
-    resolve ({ firstName, lastName });
+    resolve({ firstName, lastName });
     reject(new Error());
   });
   return promise;
