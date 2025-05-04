e5bd2cddec39e9f02859e5b8de55690a4050866e
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-05-04 15:19:42 +1000

    Add docstrings

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""This module contains a functiont hat lists all documents in a collection
+or returns an empty list if no documents exist"""
+
 
 def list_all(mongo_collection):
     """Lists all documents in a collection"""
